aa6793e88b9e43fbee9858d5957e9a2aaf823623
sameer <sameer920@gmail.com>
2018-07-29 14:41:26 -0500

    eureka integration

--- src/main/java/com/retail/ecom/inventory/controller/InventoryController.java
+++ src/main/java/com/retail/ecom/inventory/controller/InventoryController.java
@@ -1,7 +1,5 @@
 package com.retail.ecom.inventory.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +13,15 @@ public class InventoryController {
 	
 	private static int reservationId=1;
 	@RequestMapping("/availability")
-    public  ResponseEntity<?>  inventory(@RequestParam(value="id") String id) {
+    public  Inventory  inventory(@RequestParam(value="id") String id) {
 		Inventory inventory=new Inventory();
 		inventory.setId(id);
 		inventory.setInventoryCount(InventoryApplication.inventoryCount);
-		return new ResponseEntity<Inventory>(inventory, HttpStatus.OK);
+		return inventory;
     }
 	
 	@RequestMapping("/reservation")
-    public  ResponseEntity<?>  reservation(@RequestParam(value="id") String id) {
+    public  Inventory  reservation(@RequestParam(value="id") String id) {
 		Inventory inventory=new Inventory();
 		inventory.setId(id);
 		inventory.setInventoryCount(InventoryApplication.inventoryCount);
@@ -32,7 +30,7 @@ public class InventoryController {
 			inventory.setReservationID(reservationId++);
 			InventoryApplication.inventoryCount--;
 		}
-		return new ResponseEntity<Inventory>(inventory, HttpStatus.OK);
+		return inventory;
     }
 
 }
